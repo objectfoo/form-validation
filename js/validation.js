b3a291a34036c4f2461e14c890918f27939d371f
Satake <satake@objectfoo.com>
2013-08-04 21:40:19 -0700

    tighten up

--- js/validation.js
+++ js/validation.js
@@ -1,36 +1,55 @@
-/*global EventUtil, _, console*/
-(function (_, EVT) {
+/*global EventUtil, _*/
+(function (_, EventUtil) {
   'use strict';
-
   var submit,
-    inputs = _.toArray(document.forms[0].elements),
-    isText = newPredicate('text'),
-    isPassword = newPredicate('password');
+    inputs     = _.toArray(document.forms[0].elements),
 
-  // take string or array for strings
-  // return false or element matching type
-  function newPredicate(types) {
-    types = (_.isArray(types)) ? types : [types];
-    return function (el) {
-      return _.contains(types, el.type) && el;
+    // input type predicates
+    isText     = newTypePredicate('text'),
+    isPassword = newTypePredicate('password'),
+    isRequired = newTypePredicate(['password', 'text']);
+
+  // predicate: input value not empty
+  function allInputsNonEmpty(memo, input) {
+    return memo && input.value.length > 0;
+  }
+
+  // predicate: character is valid input char, delete && a-zA-Z && symbols
+  function isValidChar(c) {
+    return c === 8 || c < 128 && c > 31;
+  }
+
+  // accept string or array return false or elements matching type
+  function newTypePredicate(types) {
+    types = _.isArray(types) ? types : [types];
+    return function (elem) {
+      return _.contains(types, elem.type) && elem;
     };
   }
 
-  function textHandler(el) {
-    EVT.addHandler(el, 'keypress', function (evt) {
-      evt = EVT.getEvent(evt);
-      console.log(evt);
-      // if value is valid trigger something...
+  // TODO: accept array of predicates return reduction of results
+  function textHandler(elem) {
+    EventUtil.addHandler(elem, 'keyup', function (e) {
+      var evt = EventUtil.getEvent(e);
+
+      return isValidChar(evt.keyCode) && fieldUpdated();
     });
   }
 
-  submit = _.chain(inputs).filter(newPredicate('submit')).first().value();
+  // a field was updated
+  function fieldUpdated() {
+    submit.disabled = !_.reduce(_.filter(inputs, isRequired),
+        allInputsNonEmpty, true);
+  }
+
+  submit = _.chain(inputs).filter(newTypePredicate('submit')).first().value();
   submit.disabled = true;
 
   _.each([
     { predicate: isText, handler: textHandler },
     { predicate: isPassword, handler: textHandler }
   ], function (item) {
-    _.chain(inputs).filter(item.predicate).each(item.handler);
+    _.each(_.filter(inputs, item.predicate), item.handler);
   });
+
 })(_, EventUtil);
